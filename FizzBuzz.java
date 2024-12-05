b143709e602adeaab3db483202ee32cf2ce1d352
lidsta87 <stanic.lidija@gmail.com>
2024-12-05 13:05:34 +0100

    kommentare ergänzt

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,16 +1,29 @@
 public class FizzBuzz {
     public static void main(String[] args) {
+        // Schleife von 1 bis 100
         for (int i = 1; i <= 100; i++) {
+            // Überprüfen, ob die Zahl ein Vielfaches von 3 und 5 ist
             if (i % 3 == 0 && i % 5 == 0) {
+                // Ausgabe "FizzBuzz", wenn die Bedingung zutrifft
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            }
+            // Überprüfen, ob die Zahl nur ein Vielfaches von 3 ist
+            else if (i % 3 == 0) {
+                // Ausgabe "Fizz", wenn die Bedingung zutrifft
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            }
+            // Überprüfen, ob die Zahl nur ein Vielfaches von 5 ist
+            else if (i % 5 == 0) {
+                // Ausgabe "Buzz", wenn die Bedingung zutrifft
                 System.out.println("Buzz");
-            } else {
+            }
+            // Wenn keine der obigen Bedingungen erfüllt ist
+            else {
+                // Ausgabe der aktuellen Zahl
                 System.out.println(i);
             }
         }
     }
 }
+
 
